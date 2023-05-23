9924a854babbc3f79edee583e75e786aa39155bf
Tal Atlas <me@tal.by>
2023-05-22 22:45:15 -0400

    initial commit

---
+++ src/hs.d.ts
@@ -0,0 +1,440 @@
+/** @noSelfInFile */
+
+/**
+ * Outputs text to the hammerspoon console
+ */
+declare function print(o: any): void
+
+/**
+ * Tries to convert its argument to a number. If the argument is already a number or a string convertible to a number, then tonumber returns this number; otherwise, it returns nil.
+ *
+ * @param val Value to convert to a number
+ * @param base Default: 10; An optional argument specifies the base to interpret the numeral. The base may be any integer between 2 and 36, inclusive. In bases above 10, the letter 'A' (in either upper or lower case) represents 10, 'B' represents 11, and so forth, with 'Z' representing 35. In base 10 (the default), the number can have a decimal part, as well as an optional exponent part (see §2.1). In other bases, only unsigned integers are accepted.
+ */
+declare function tonumber(val: any, base?: number | string): undefined | number
+
+declare module os {
+  export function getenv(this: void, path: string): string
+}
+
+declare module hs {
+  export const alert: Alert
+  export const application: SpoonApplications
+  export const drawing: {
+    color: Color
+  }
+  export const http: HTTP
+  export const json: Json
+  export const task: Tasks
+  export const screen: SpoonScreen
+  export const timer: Timers
+  export const eventtap: EventTaps
+  export const hotkey: Hotkeys
+  export const keycodes: {
+    map: {[k: number]: string}
+  }
+  export const spotify: SpoonSpotify
+  export const notify: Notify
+
+  /**
+   * Runs a shell command, optionally loading the users shell environment first, and returns stdout as a string, followed by the same result codes as os.execute would return.
+   *
+   */
+  export const execute: (command: string | string[], with_user_env?: boolean) => LuaMultiReturn<[string, string, string, string]>
+}
+
+declare type UUID = string
+
+declare type DefaultStyle = {
+  /**  a number specifying the font size to be used for the alert text, defaults to 27. */
+  textSize?: number
+  fillColor?: RGBColorRepresentation | HSBColorRepresentation
+}
+
+// --- hs.alert ---
+// https://www.hammerspoon.org/docs/hs.alert.html
+
+/** @noSelf */
+declare class Alert {
+  defaultStyle: DefaultStyle
+  closeAll(seconds?: number): void
+  closeSpecific(uuid: UUID, seconds?: number): void
+
+  show(str: string, seconds: number): UUID
+  show(str: string, style?: DefaultStyle, screen?: never, seconds?: number): UUID
+}
+
+// --- hs.drawing.color ---
+// https://www.hammerspoon.org/docs/hs.drawing.color.html
+
+declare type RGBColor = {
+  /** the red component of the color specified as a number from 0.0 to 1.0. */
+  red: number,
+  /** the green component of the color specified as a number from 0.0 to 1.0. */
+  green: number,
+  /** the blue component of the color specified as a number from 0.0 to 1.0 */
+  blue: number,
+  /** the color transparency from 0.0 (completely transparent) to 1.0 (completely opaque) */
+  alpha: number,
+}
+
+declare type RGBColorRepresentation = RGBColor
+
+declare type HSBColor = {
+  /** the hue component of the color specified as a number from 0.0 to 1.0. */
+  hue: number,
+  /** the saturation component of the color specified as a number from 0.0 to 1.0. */
+  saturation: number,
+  /** the brightness component of the color specified as a number from 0.0 to 1.0. */
+  brightness: number,
+  /** the color transparency from 0.0 (completely transparent) to 1.0 (completely opaque) */
+  alpha: number,
+}
+
+declare type HSBColorRepresentation = HSBColor
+
+/** @noSelf */
+declare class Color {
+  ansiTerminalColors: any
+  /** This table contains a collection of various useful pre-defined colors */
+  hammerspoon: {
+    /** The same red used for OS X window close buttons */
+    osx_red: any,
+    /** The same green used for OS X window zoom buttons */
+    osx_green: any,
+    /** The same yellow used for OS X window minimize buttons */
+    osx_yello: any,
+  }
+  /** A collection of colors representing the X11 color names as defined at https://en.wikipedia.org/wiki/Web_colors#X11_color_names (names in lowercase) */
+  x11: any
+  /** Returns a table containing the HSB representation of the specified color. */
+  asRGB(color: RGBColor): RGBColorRepresentation
+  asHSB(color: HSBColor): HSBColorRepresentation
+  colorsFor(list: string[]): any | null
+  lists(): any
+}
+
+// --- hs.http ---
+// https://www.hammerspoon.org/docs/hs.http.html
+
+/** @noSelf */
+declare class HTTP {
+  asyncGet(
+    url: string,
+    headers: { [key: string]: string } | null,
+    callback: (
+      this: void,
+      httpStatus: number,
+      body: string,
+      headers: { [key: string]: string }
+    ) => void
+  ): any
+  urlParts(url: string): {
+    baseURL: string,
+    absoluteString: string,
+    absoluteURL: string,
+    fileSystemRepresentation: string,
+    fragment: string,
+    host: string,
+    isFileURL: boolean,
+    lastPathComponent: string,
+    parameterString: string,
+    password: string,
+    path: string,
+    pathComponents: string,
+    pathExtension: string,
+    port: string,
+    query: string,
+    queryItems: { [key: string]: any },
+    relativePath: string,
+    relativeString: string,
+    resourceSpecifier: string,
+    scheme: string,
+    standardizedURL: string,
+    user: string,
+  }
+}
+
+// --- hs.json ---
+// https://www.hammerspoon.org/docs/hs.json.html
+
+/** @noSelf */
+declare class Json {
+  /** Decodes JSON into a table */
+  decode<T = {}>(jsonString: string): T
+  /**Encodes a table as JSON */
+  encode<T = {}>(table: T, prettyPrint?: boolean): string
+  /** Decodes JSON file into a table. */
+  read<T = {}>(path: string): T | null
+  /** Encodes a table as JSON to a file */
+  write<T = {}>(data: T, path: string, prettyPrint?: boolean, replace?: boolean): boolean
+}
+
+// --- hs.task ---
+// https://www.hammerspoon.org/docs/hs.task.html
+
+declare class Tasks {
+  new(
+    this: void,
+    launchPath: string,
+    callbackFn: (this: void, exitCode: number, stdOut: string, stdErr: string) => void,
+    streamCallbackFn?: (this: void, task: Tasks | null, stdOut: string, stdErr: string) => void,
+    arguments?: string[]
+  ): Task
+  new(
+    this: void,
+    launchPath: string,
+    callbackFn: (this: void, exitCode: number, stdOut: string, stdErr: string) => void,
+    arguments?: string[]
+  ): Task
+}
+
+declare interface Task {
+  terminate(): Task
+  start(): Task | false
+  setWorkingDirectory(dir: string): Task
+}
+
+
+// --hs.screen ---
+// https://www.hammerspoon.org/docs/hs.screen.html
+
+declare class SpoonScreen {
+  mainScreen(this: void): SpoonScreen
+  desktopImageURL(imageURL?: string): SpoonScreen | string
+}
+
+// --- hs.timer ---
+// https://www.hammerspoon.org/docs/hs.timer.html
+
+declare type Timers = {
+  doAfter: (this: void, sec: number, fn: () => void) => Timer
+  doEvery: (this: void, interval: number, fn: () => void) => Timer
+  setNextTrigger: (this: void, seconds: number) => Timer
+}
+
+declare type Timer = {
+  /**
+   * Immediately fires a timer
+   *  * This cannot be used on a timer which has already stopped running
+   */
+  fire: () => Timer
+
+  /**
+   * Returns the number of seconds until the timer will next trigger
+   *  * The return value may be a negative integer in two circumstances:
+   *  * Hammerspoon's runloop is backlogged and is catching up on missed timer triggers
+   *  * The timer object is not currently running. In this case, the return value of this method is the number of seconds since the last firing (you can check if the timer is running or not, with hs.timer:running()
+   */
+  nextTrigger: () => number
+
+  /**
+   * Sets the next trigger time of a timer
+   *  * If the timer is not already running, this will start it
+   * @param seconds A number of seconds after which to trigger the timer
+   * @returns The `hs.timer` object, or nil if an error occurred
+   */
+  setNextTrigger: (seconds: number) => Timer | null
+
+  /**
+   * Starts an `hs.timer` object
+   *  * The timer will not call the callback immediately, the timer will wait until it fires
+   *  * If the callback function results in an error, the timer will be stopped to prevent repeated error notifications (see the `continueOnError` parameter to `hs.timer.new()` to override this)
+   */
+  start: () => Timer
+  stop: () => Timer
+}
+
+type EventTapTypeNames =
+  'nullEvent' |
+  'leftMouseDown' |
+  'leftMouseUp' |
+  'rightMouseDown' |
+  'rightMouseUp' |
+  'mouseMoved' |
+  'leftMouseDragged' |
+  'rightMouseDragged'|
+  'keyDown' |
+  'keyUp' |
+  'flagsChanged' |
+  'scrollWheel' |
+  'tabletPointer' |
+  'tabletProximity' |
+  'otherMouseDown' |
+  'otherMouseUp' |
+  'otherMouseDragged'
+
+declare type KeboardModifiers =
+  "⌥" |
+  "⌃" |
+  "⌘" |
+  "⇧" |
+  "cmd"|
+  "command" |
+  "alt"|
+  "option" |
+  "shift"|
+  "ctrl"|
+  "control" |
+  "rightCmd"|
+  "rightAlt"|
+  "rightShift"|
+  "rightCtrl"|
+  "fn"
+
+declare type EventTapTypes = number
+
+declare type SpoonEvents = {
+  newEvent: () => SpoonEvent
+  types: { [k in EventTapTypeNames]: EventTapTypes }
+}
+declare type SpoonEvent = {
+  getType: (nsSpecificType?: boolean) => EventTapTypes
+  setType: (type: EventTapTypes) => SpoonEvent
+  getKeyCode: () => number
+  getFlags: () => {[k in KeboardModifiers]: boolean}
+}
+
+type SpoonEventTapCallback =
+  (ev: SpoonEvent) =>
+    void |
+    boolean |
+    [boolean, SpoonEvent[]]
+
+declare type EventTaps = {
+  new: (this: void, types: [EventTapTypes | 'all', ...EventTapTypes[]], fn: SpoonEventTapCallback) => EventTap
+  event: SpoonEvents
+}
+
+declare type EventTap = {
+  /**
+   * Determine whether or not an event tap object is enabled.
+   */
+  isEnabled(): boolean
+
+  /**
+   * Starts an event tap
+   */
+  start(): EventTap
+
+  /**
+   * Stops an event tap
+   */
+  stop(): EventTap
+}
+
+declare interface Hotkeys {
+  new: (
+    this: void,
+    mods: KeboardModifiers[],
+    key: string | number,
+    message?: string,
+    pressedfn?: () => void,
+    releasedfn?: () => void,
+    repeatfn?: () => void
+  ) => Hotkey
+
+  bind: (
+    this: void,
+    mods: KeboardModifiers[],
+    key: string | number,
+    message?: string,
+    pressedfn?: () => void,
+    releasedfn?: () => void,
+    repeatfn?: () => void
+  ) => Hotkey
+}
+
+declare type Hotkey = {
+  /**
+   * When you enable a hotkey that uses the same keyboard
+   * combination as another previously-enabled hotkey,
+   * the old one will stop working as it's being "shadowed"
+   * by the new one. As soon as the new hotkey is disabled
+   * or deleted the old one will trigger again.
+   */
+  enable: () => Hotkey | null
+  disable: () => Hotkey
+  delete: () => void
+}
+
+declare type SpoonApplications = {
+  /**
+   * Launches the app with the given name, or activates it if it's already running
+   * @param name name - A string containing the name of the application to either launch or focus. This can also be the full path to an application (including the .app suffix) if you need to uniquely distinguish between applications in different locations that share the same name
+   * @returns True if the application was either launched or focused, otherwise false (e.g. if the application doesn't exist)
+   */
+  launchOrFocus: (this: void, name: string) => boolean
+
+  /**
+   * Finds running applications
+   * @param hint search criterion for the desired application(s); it can be: - a pid number as per `hs.application:pid()` - a bundle ID string as per `hs.application:bundleID()` - a string pattern that matches (via `string.find`) the application name as per `hs.application:name()` (for convenience, the matching will be done on lowercased strings) - a string pattern that matches (via `string.find`) the application's window title per `hs.window:title()` (for convenience, the matching will be done on lowercased strings)
+   * @param exact a boolean, true to check application names for exact matches, false to use Lua's string:find() method. Defaults to false
+   * @param stringLiteral a boolean, true to interpret the hint string literally, false to interpret it as a Lua Pattern. Defaults to false.
+   * @returns one or more hs.application objects for running applications that match the supplied search criterion, or nil if none found
+   */
+  find: (
+    hint: number | string,
+    exact?: boolean,
+    stringLiteral?: boolean
+  ) => SpoonApplication | SpoonApplication[] | null
+}
+
+declare type SpoonApplication = {}
+
+declare type SpoonSpotify = {
+  state_paused: string
+  state_playing: string
+  state_stopped: string
+
+  /**
+   * Displays information for current track on screen
+   */
+  displayCurrentTrack: (this: void) => void
+
+  /**
+   * Skips the playback position forwards by 5 seconds
+   */
+  ff: (this: void) => void
+
+  /**
+   * Skips the playback position forwards by 5 seconds
+   */
+  rw: (this: void) => void
+
+  /**
+   * Sets the playback position in the current song
+   * @param pos A number containing the position (in seconds) to jump to in the current song
+   */
+  setPosition: (this: void, pos: number) => void
+
+  /**
+   * Gets the playback position (in seconds) in the current song
+   * @returns A number indicating the current position in the song
+   */
+  getPosition: (this: void) => number
+
+  next: (this: void) => void
+  previous: (this: void) => void
+  pause: (this: void) => void
+  play: (this: void) => void
+  playpause: (this: void) => void
+}
+
+declare interface Notify {
+  /**
+   * Shorthand constructor to create and show simple notifications
+   * * All three textual parameters are required, though they can be empty strings
+   * * This function is really a shorthand for hs.notify.new(...):send()
+   * * Notifications created using this function will inherit the default withdrawAfter value, which is 5 seconds. To produce persistent notifications you should use hs.notify.new() with a withdrawAfter attribute of 0.
+   *
+   * @param title the title for the notification
+   * @param subTitle the subtitle, or second line, of the notification
+   * @param body the main textual body of the notification
+   */
+  show(this: void, title: string, subTitle: string, body: string): Notification
+}
+
+declare interface Notification {
+
+}
---
+++ src/init.ts
@@ -0,0 +1,172 @@
+type InvocationDeifinitionSet = Record<string, InvocationDefinition>
+
+type InvocationDefinition = {
+  desc: string
+  key: string
+  appName?: string
+  subInvocations?: InvocationDeifinitionSet
+}
+
+const BaseDefinitions = {
+  'a': {
+    desc: "Messages",
+    key: 'a',
+    appName: "Messages.app",
+    subInvocations: {
+      'w': {
+        desc: "WhatsApp",
+        key: "w",
+        appName: "WhatsApp.app"
+      }
+    }
+  },
+  'c': {
+    desc: "Chat",
+    key: 'c',
+    appName: "Slack.app"
+  },
+  'e': {
+    desc: "Email",
+    key: 'e',
+    appName: 'Mimestream.app'
+  },
+  'm': {
+    desc: "Spotify",
+    key: 'm',
+    appName: "Spotify.app"
+  },
+  'w': {
+    desc: "Arc",
+    key: 'w',
+    appName: "Arc.app",
+    subInvocations: {
+      's': {
+        desc: "Safari",
+        key: "s",
+        appName: "Safari.app"
+      }
+    }
+  }
+} as const satisfies InvocationDeifinitionSet
+
+let currentDefinitions: InvocationDeifinitionSet = BaseDefinitions
+
+const invocationTap = hs.eventtap.new([hs.eventtap.event.types.keyDown], (ev) => {
+  if (ev && ev.getType() === hs.eventtap.event.types.keyDown) {
+    const key = hs.keycodes.map[ev.getKeyCode()]
+
+    if (key == 'z' && ev.getFlags().ctrl) {
+      currentDefinitions = BaseDefinitions
+      return true
+    }
+
+    const definition = currentDefinitions[key]
+    if (definition != null) {
+      hs.alert.closeAll()
+      hs.alert.show(`${key} - ${definition.desc}`, 0.5)
+      if (definition.appName) {
+        hs.application.launchOrFocus(definition.appName)
+      }
+    }
+
+    invocationTap.stop()
+    return true
+  }
+
+  return false
+})
+
+const ctrlZHotkey = hs.hotkey.new(["⌃"], "z", undefined, () => {
+  currentDefinitions = BaseDefinitions
+  hs.alert.show("⌃z", 0.5)
+  if (!invocationTap.isEnabled()) {
+    invocationTap.start()
+  }
+  hs.timer.doAfter(1, () => {
+    invocationTap.stop()
+  })
+})
+
+currentDefinitions = BaseDefinitions
+invocationTap.stop()
+ctrlZHotkey.enable()
+
+
+hs.hotkey.bind(['⌃'], '`', undefined, () => {
+  hs.application.launchOrFocus('Warp.app')
+})
+
+function spotifySkipPosition(seconds: number): void {
+  const pos = hs.spotify.getPosition()
+  const newPos = tonumber(pos + seconds)
+
+  if (newPos) {
+    hs.spotify.setPosition(newPos)
+  }
+  else {
+    print(`Error newPos: ${hs.json.encode(newPos)}`)
+  }
+}
+
+hs.hotkey.bind(['⌥','⌃'], 'right', undefined, () => {
+  spotifySkipPosition(20)
+})
+hs.hotkey.bind(['⌥','⌃'], 'left', undefined, () => {
+  spotifySkipPosition(-20)
+})
+hs.hotkey.bind(['⌥','⌃','⇧'], 'right', undefined, () => {
+  hs.spotify.next()
+})
+hs.hotkey.bind(['⌥','⌃','⇧'], 'left', undefined, () => {
+  hs.spotify.previous()
+})
+
+function sendSpotifyCommand(cmd: 'promote' | 'demotes' | 'promotes') {
+  let command = "/opt/homebrew/bin/tea"
+  let args = ["+nodejs.org^14", "yarn", "run", "cli", cmd]
+  const task = hs.task.new(
+    command,
+    (exitCode, stdOut, stdErr) => {
+      // print(`stdOut: ${stdOut.substring(0,100)}`)
+      // print(`stdErr: ${stdErr.substring(0,100)}`)
+      const startStr = "body: '"
+      let idxStart = stdOut.indexOf(startStr)
+      let idxEnd = stdOut.indexOf("' }\nDone")
+
+      if (idxStart >= 0 && idxEnd >= 0) {
+        const jsonText = stdOut.substring(idxStart + startStr.length, idxEnd)
+        const result: any = hs.json.decode(jsonText)
+        const first = result.result && result.result[1]
+        print(hs.json.encode({first}))
+
+        if (first) {
+          hs.notify.show("Spotify Command Complete", cmd, first.reason)
+          return
+        }
+      }
+      if (exitCode && exitCode > 0) {
+        hs.notify.show("Spotify Command Error", cmd, stdErr)
+      }
+      else {
+        hs.notify.show("Spotify Command Complete", "", stdOut)
+      }
+    },
+    args
+  )
+
+  task.setWorkingDirectory("/Users/tal/Projects/spotify-playlist")
+
+  return task
+}
+hs.hotkey.bind(['⌥','⌃'], 'up', undefined, () => {
+  hs.alert.show("Promote")
+  sendSpotifyCommand('promote').start()
+})
+hs.hotkey.bind(['⌥','⇧', '⌃'], 'up', undefined, () => {
+  hs.alert.show("Promote and skip")
+  sendSpotifyCommand('promotes').start()
+})
+hs.hotkey.bind(['⌥','⌃'], 'down', undefined, () => {
+  hs.alert.show("demote")
+  sendSpotifyCommand('demotes').start()
+})
